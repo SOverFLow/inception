af86f1fbbb38ce1c21d4123e61e9395642f91eba
salah <codersalah0@gmail.com>
2022-12-10 07:41:33 -0500

    edit

--- srcs/requirements/mariadb/tools/mariadb.sql
+++ srcs/requirements/mariadb/tools/mariadb.sql
@@ -2,3 +2,4 @@ create database wordpresss;
 create user "wpuser"@"%" identified by "Selhanda@123";
 grant all privileges on wordpresss.* to 'wpuser'@'%';
 flush privileges;
+alter user 'root'@'localhost' identified by 'Selhanda@123'
