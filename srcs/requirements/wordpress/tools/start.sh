d8ae2fb2db2c5719ac0d545028c3664907a32b94
root <codersalah0@gmail.com>
2022-12-11 11:40:37 -0500

    edit

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-wp config create --dbname=wordpresss --dbuser=wpuser --dbpass=Selhanda@123 --dbhost=mariadb --allow-root
+wp config create --dbname=wordpresss \
+    --dbuser=wpuser --dbpass=Selhanda@123 \
+    --dbhost=mariadb --allow-root
+
+
+wp core install --url="selhanda.com" \
+ --title="WordPress" \
+ --admin_user="root" \
+ --admin_password="Selhanda@123" \
+ --admin_email="admin@email.com" \
+ --allow-root
 
 php-fpm7.3 -R -F
