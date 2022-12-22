5177a2c9753f201f93e48dfc49da2d2ece13ded9
salah <codersalah0@gmail.com>
2022-12-22 07:48:24 -0500

    edit

--- srcs/requirements/mariadb/tools/mariadb.sql
+++ srcs/requirements/mariadb/tools/mariadb.sql
@@ -1,5 +1,5 @@
-create database wordpresss;
+create database wordpress;
 create user "wpuser"@"%" identified by "Selhanda@123";
-grant all privileges on wordpresss.* to 'wpuser'@'%';
+grant all privileges on wordpress.* to 'wpuser'@'%';
 flush privileges;
 alter user 'root'@'localhost' identified by 'Selhanda@123'
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-wp config create --dbname=wordpresss \
+wp config create --dbname=wordpress \
     --dbuser=wpuser --dbpass=Selhanda@123 \
     --dbhost=mariadb --allow-root
 
 
-wp core install --url="selhanda.42.fr" \
+wp core install --url="10.12.177.171" \
  --title="inception" \
  --admin_user="root" \
  --admin_password="Selhanda@123" \
  --admin_email="admin@email.com" \
  --allow-root
+ 
 
 wp plugin install redis-cache --activate --allow-root
 wp config set WP_REDIS_PORT "6379" --allow-root
