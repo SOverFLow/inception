70da373371ec5d6e38c48176ab5c06869ad31ca1
salah <codersalah0@gmail.com>
2023-01-13 08:05:54 -0500

    edit

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 service mysql start
-mysql -e "CREATE DATABASE ${DB_NAME};"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
-# mysql -e "ALTER USER '${ADMIN_USER}'@'%' IDENTIFIED BY '${DB_PASS}'"
+mysql -e "ALTER USER '${ADMIN_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}'"
 
-service mysql start
-
-mysqld
