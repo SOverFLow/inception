27e5705c2d18b7bac65ced8d26d7f31e9ab6ea08
salah <codersalah0@gmail.com>
2023-01-13 07:05:39 -0500

    edit

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -5,7 +5,8 @@ mysql -e "CREATE DATABASE ${DB_NAME};"
 mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
+# mysql -e "ALTER USER '${ADMIN_USER}'@'%' IDENTIFIED BY '${DB_PASS}'"
 
-service mysql stop
+service mysql st
 
 mysqld
